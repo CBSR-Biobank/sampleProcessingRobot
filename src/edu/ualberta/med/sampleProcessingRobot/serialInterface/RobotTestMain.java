db309f29a110b99825869558830552e3597d1372
kory <korymath@gmail.com>
2010-06-08 15:54:20 -0600

    working, added some comments

--- src/edu/ualberta/med/sampleProcessingRobot/serialInterface/RobotTestMain.java
+++ src/edu/ualberta/med/sampleProcessingRobot/serialInterface/RobotTestMain.java
@@ -9,14 +9,14 @@ public class RobotTestMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		String barcode = null;
 		double volume = 0;
-		
+
 		// New interface
 		RobotInterface robot = new RobotInterface();
-		try{
-			robot.initialize(SourceTubeSize.TEN_MIL, 
+		try {
+			robot.initialize(SourceTubeSize.TEN_MIL,
 					FluidLevelType.FLUID_TYPE_BLOOD);
 		} catch (RobotInitException cbsre) {
 			System.out.println(cbsre);
@@ -25,105 +25,110 @@ public class RobotTestMain {
 			System.out.println(cbse);
 			System.exit(1);
 		}
+
 		// Get tube
-		try{
+		try {
 			robot.pickupSourceTube(1);
-		} catch(RobotException cbsre) {
+		} catch (RobotException cbsre) {
 			System.out.println(cbsre);
 			done(robot);
 			System.exit(1);
 		}
 		// Read barcode
-		try{
+		try {
 			barcode = robot.scanSourceTubeBarcode();
-		} catch(RobotException cbsre) {
+		} catch (RobotException cbsre) {
 			System.out.println(cbsre);
 			done(robot);
 			System.exit(1);
 		}
-		if(barcode == "") {
+		if (barcode == "") {
 			System.out.println("Couldn't read source tube barcode");
 			done(robot);
 			System.exit(1);
-		}
-		else
+		} else
 			System.out.println("Tube barcode is: " + barcode);
 		// Get fluid level
-		try{
+		try {
 			volume = robot.scanSourceTubeLevel();
-		} catch(RobotException cbsre) {
+		} catch (RobotException cbsre) {
 			System.out.println(cbsre);
 			done(robot);
 			System.exit(1);
 		}
 		System.out.println("Tube fluid level is: " + volume + "uL");
 		// Grip source tube
-		try{
+		try {
 			robot.gripSourceTube();
-		} catch(RobotException cbsre) {
+		} catch (RobotException cbsre) {
 			System.out.println(cbsre);
 			done(robot);
 			System.exit(1);
 		}
 		// Get pipette tip
-		try{
+		try {
 			robot.newPipetteTip();
-		} catch(RobotException cbsre) {
+		} catch (RobotException cbsre) {
 			System.out.println(cbsre);
 			done(robot);
 			System.exit(1);
 		}
 		int sampleID = 1;
 		// Get some liquid
-		try{
+		try {
 			volume = robot.aspirateSample(800);
-		} catch(RobotException cbsre) {
+		} catch (RobotException cbsre) {
 			System.out.println(cbsre);
 			done(robot);
 			System.exit(1);
 		}
-		
-		try{
+
+		try {
 			robot.dispenseFTA();
 			robot.dispenseFTA();
-		} catch(RobotException cbsre) {
+		} catch (RobotException cbsre) {
 			System.out.println(cbsre);
 			done(robot);
 			System.exit(1);
 		}
-		
 
 		// Whittle down most of the liquid
-		while(volume > 3000) {
+		// Only do this 6 times, regardless of the volume
+		// This can be dependent on volume in the future
+
+		// while (volume > 3000) {
+		int count = 1;
+		while (count < 7) {
 			// Get some liquid
-			try{
+			try {
 				volume = robot.aspirateSample(800);
-			} catch(RobotException cbsre) {
+			} catch (RobotException cbsre) {
 				System.out.println(cbsre);
 				done(robot);
 				System.exit(1);
 			}
 			// Dispense to samples
-			try{
+			try {
 				robot.dispensePallet(1, sampleID++, 400);
 				robot.dispensePallet(1, sampleID++, 400);
-			} catch(RobotException cbsre) {
+			} catch (RobotException cbsre) {
 				System.out.println(cbsre);
 				done(robot);
 				System.exit(1);
 			}
+			count++;
 		}
 		// Clean up
-		try{
+		try {
 			robot.disposePipetteTip();
 			robot.returnSourceTube();
 			robot.homeArm();
-		} catch(RobotException cbsre) {
+		} catch (RobotException cbsre) {
 			System.out.println(cbsre);
 			done(robot);
 			System.exit(1);
 		}
-		System.out.println("Success!");			
+		System.out.println("Success!");
 	}
 
 	public static void done(RobotInterface robot) {
@@ -134,7 +139,7 @@ public class RobotTestMain {
 			System.out.println("Could not cleanly shutdown robot!");
 			System.exit(1);
 		}
-		
+
 	}
 
 }
